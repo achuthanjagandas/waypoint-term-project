39859bfb429d82589c0aa26d16c9d4db41124601
Achuthan Jagandas <achuthanjagandas@gmail.com>
2026-08-05 19:56:49 -0400

    week 13: add Park model and Trail relationship

--- trails/models.py
+++ trails/models.py
@@ -4,6 +4,24 @@ from django.core.validators import MinValueValidator
 from django.db import models
 
 
+class Park(models.Model):
+    """Store a park that contains one or more trails."""
+
+    name = models.CharField(
+        max_length=150,
+    )
+    region = models.CharField(
+        max_length=150,
+    )
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        """Return a readable park name and region."""
+        return f"{self.name} ({self.region})"
+
+
 class Trail(models.Model):
     """Store a trail that can be displayed in the Waypoint catalog."""
 
@@ -14,6 +32,13 @@ class Trail(models.Model):
 
     name = models.CharField(
         max_length=150,
+    )
+    park = models.ForeignKey(
+        Park,
+        on_delete=models.PROTECT,
+        related_name="trails",
+        null=True,
+        blank=True,
     )
     distance_km = models.DecimalField(
         max_digits=6,
